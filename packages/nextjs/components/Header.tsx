035250dec1da3931646898198d743fa9a0923e6b
Ana Cristina Alves Jardim <ana.jardim@sou.inteli.edu.br>
2025-11-07 23:19:30 -0300

    feat: add logo to the header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -60,8 +60,13 @@ export const Header = () => {
   });
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md px-0 sm:px-2 border-b border-base-300">
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md px-0 sm:px-2 border-b border-base-300 py-0 !py-0">
       <div className="navbar-start w-auto lg:w-1/2">
+        <Link href="/" passHref className="lg:hidden flex items-center gap-0 ml-1 shrink-0 !py-0">
+          <div className="flex relative w-48 h-16 -my-1">
+            <Image alt="OnLicit logo" className="cursor-pointer object-contain" fill src="/OnLicit.png" />
+          </div>
+        </Link>
         <details className="dropdown" ref={burgerMenuRef}>
           <summary className="ml-1 btn btn-ghost lg:hidden hover:bg-transparent">
             <Bars3Icon className="h-1/2" />
@@ -81,13 +86,9 @@ export const Header = () => {
             </li>
           </ul>
         </details>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
-            <Image alt="App logo" className="cursor-pointer" fill src="/logo.svg" />
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold leading-tight">OnLicit</span>
-            <span className="text-xs">Online Bids</span>
+        <Link href="/" passHref className="hidden lg:flex items-center gap-0 ml-2 mr-3 shrink-0 !py-0">
+          <div className="flex relative w-64 h-24 -my-1">
+            <Image alt="OnLicit logo" className="cursor-pointer object-contain" fill src="/OnLicit.png" />
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
